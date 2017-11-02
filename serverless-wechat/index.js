b8775ea4d4a92620d356461336bf1883ae1bb82a
Phodal Huang <h@phodal.com>
2017-11-02 09:28:35 +0800

    [WECHAT] init vars

--- serverless-wechat/index.js
+++ serverless-wechat/index.js
@@ -1,9 +1,54 @@
 const express = require('express')
 const app = express()
+let wechat = require('wechat');
+let config = {
+  token: process.env.TOKEN,
+  appid: process.env.APP_ID,
+  encodingAESKey: process.env.AESKey,
+  checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
+};
 
 app.get('/', function (req, res) {
       res.send('Hello World!')
 })
+
+app.use(express.query());
+app.use('/wechat', wechat(config, function (req, res, next) {
+  // 微信输入信息都在req.weixin上
+  let message = req.weixin;
+  if (message.FromUserName === 'diaosi') {
+    // 回复屌丝(普通回复)
+    res.reply('hehe');
+  } else if (message.FromUserName === 'text') {
+    //你也可以这样回复text类型的信息
+    res.reply({
+      content: 'text object',
+      type: 'text'
+    });
+  } else if (message.FromUserName === 'hehe') {
+    // 回复一段音乐
+    res.reply({
+      type: "music",
+      content: {
+        title: "来段音乐吧",
+        description: "一无所有",
+        musicUrl: "http://mp3.com/xx.mp3",
+        hqMusicUrl: "http://mp3.com/xx.mp3",
+        thumbMediaId: "thisThumbMediaId"
+      }
+    });
+  } else {
+    // 回复高富帅(图文回复)
+    res.reply([
+      {
+        title: '你来我家接我吧',
+        description: '这是女神与高富帅之间的对话',
+        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+        url: 'http://nodeapi.cloudfoundry.com/'
+      }
+    ]);
+  }
+}));
 
 // app.listen(8080);
 module.exports = app;
