6e43e37ef61db6b001cb52e5b88e1afd9d2fc149
Phodal Huang <h@phodal.com>
2017-11-03 15:41:25 +0800

    [SNS] update config

--- ses/handler.js
+++ ses/handler.js
@@ -1,11 +1,7 @@
 'use strict';
 
-// importing AWS sdk
 import AWS from 'aws-sdk';
 import request from 'request';
-// importing config file which contains AWS key
-// Best practice: to use a config.copy.json when pushing to github
-// Coz exposing the AWS keys to public is not good
 import config from './config.json';
 
 AWS.config.update({
@@ -14,66 +10,7 @@ AWS.config.update({
   region: config.aws.region
 });
 
-// Instatiating the SES from AWS SDK
 let ses = new AWS.SES();
-
-// Structure of sendMail params structure:
-/*
-var params = {
-  Destination: {  / required /
-    BccAddresses: [
-      'STRING_VALUE',
-      / more items /
-    ],
-    CcAddresses: [
-      'STRING_VALUE',
-      / more items /
-    ],
-    ToAddresses: [
-      'STRING_VALUE',
-      / more items /
-    ]
-  },
-  Message: { / required /
-    Body: { / required /
-      Html: {
-        Data: 'STRING_VALUE', / required /
-        Charset: 'STRING_VALUE'
-      },
-      Text: {
-        Data: 'STRING_VALUE', / required /
-        Charset: 'STRING_VALUE'
-      }
-    },
-    Subject: { / required /
-      Data: 'STRING_VALUE', / required /
-      Charset: 'STRING_VALUE'
-    }
-  },
-  Source: 'STRING_VALUE', / required /
-  ConfigurationSetName: 'STRING_VALUE',
-  ReplyToAddresses: [
-    'STRING_VALUE',
-    / more items /
-  ],
-  ReturnPath: 'STRING_VALUE',
-  ReturnPathArn: 'STRING_VALUE',
-  SourceArn: 'STRING_VALUE',
-  Tags: [
-    {
-      Name: 'STRING_VALUE', / required /
-      Value: 'STRING_VALUE' / required /
-    },
-    / more items /
-  ]
-};
-
-ses.sendEmail(params, function(err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else     console.log(data);           // successful response
-});
-
-*/
 
 // The function to send SES email message
 module.exports.sendMail = (event, context, callback) => {
@@ -88,12 +25,10 @@ module.exports.sendMail = (event, context, callback) => {
   let sourceEmail = event.body.sourceEmail;
   let replyToAddresses = event.body.replyToAddresses;
 
-// Building the slack message
   var options = {
     text: 'We have got a customer support from ' + replyToAddresses + ' Log into <https://privateemail.com/appsuite/> to answer their query.',
   }
 
-// The parameters for sending mail using ses.sendEmail()
   let emailParams = {
     Destination: {
       BccAddresses: bccEmailAddresses,
@@ -116,7 +51,6 @@ module.exports.sendMail = (event, context, callback) => {
     ReplyToAddresses: replyToAddresses
   };
 
-// the response to send back after email success.
   const response = {
     statusCode: 200,
     body: JSON.stringify({
@@ -124,7 +58,6 @@ module.exports.sendMail = (event, context, callback) => {
     }),
   };
 
-// The sendEmail function taking the emailParams and sends the email requests.
   ses.sendEmail(emailParams, function (err, data) {
       if (err) {
           console.log(err, err.stack);
@@ -133,16 +66,7 @@ module.exports.sendMail = (event, context, callback) => {
         console.log("SES successful");
         console.log(data);
 
-        request.post(config.slackWebhook, { body: JSON.stringify(options)}, function (err, httpResponse, body) {
-          if (err) {
-            console.error('Slack webhook failed:', err);
-            callback(err);
-          }
-          console.log('Post to slack bot successful!!');
-          console.log(httpResponse);
-          console.log('Post to slack bot replied with:', body);
-          callback(null, response);
-        });
+        callback(null, response);
       }
   });
 };
