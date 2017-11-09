62acd5bbe6b938c78272efa7560c39ab8baa7922
Phodal Huang <h@phodal.com>
2017-11-09 10:57:04 +0800

    [TEST] update dao test

--- dynamodb-test/test/dao.test.js
+++ dynamodb-test/test/dao.test.js
@@ -8,27 +8,31 @@ const dynamoDb = require('../todos/dynamodb');
 process.env.TABLE_NAME = 'SomeTable';
 
 describe('Test ToDo DAO - save', () => {
-  it('should save todo', () => {
+  it('should save todo', (done) => {
     AWS.mock('DynamoDB.DocumentClient', 'put', (params, callback) => {
+      console.log(params);
       params.should.be.an.Object();
       params.should.have.property('Item');
       params.Item.should.have.property('id', 'todoId');
       params.Item.should.have.property('text', 'ToDoText');
-      params.Item.should.have.property('state', 'OPEN');
+      params.Item.should.have.property('checked', false);
       params.should.have.property('TableName', 'SomeTable');
 
       callback(null, {});
+      done()
     });
 
-    return dynamoDb.put({}, (error) => {
-      console.log(error);
-      if (error) {
-        return;
-      }
-
-      console.log('-');
-      callback(null, {});
+    return dynamoDb.put({
+      TableName: 'SomeTable',
+      Item: {
+        id: 'todoId',
+        text: 'ToDoText',
+        checked: false
+      },
+    }).promise().then((data) =>{
+      console.log(data);
     })
+
   });
 
   afterEach(() => {
